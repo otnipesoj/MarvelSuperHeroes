a3a4715bc575aef2f100e203dc0f1775339e135c
Jose Luis Caldeira Pinto <otnipesoj@gmail.com>
2020-09-03 11:31:34 +0100

    Use stack view. Favorite button not showing.

--- MarvelSuperHeroes/Custom Views/ViewControllers/MarvelCharacterDetailsHeaderViewController.swift
+++ MarvelSuperHeroes/Custom Views/ViewControllers/MarvelCharacterDetailsHeaderViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class MarvelCharacterDetailsHeaderViewController: UIViewController {
     
+    let stackView = UIStackView()
     let characterThumbnail = MarvelCharacterThumbnail(frame: .zero)
     let characterNameLabel = MarvelTitleLabel(textAlignment: .center, fontSize: 20)
     let favouriteButton = MarvelFavoriteButton(frame: .zero)
@@ -23,39 +24,63 @@ class MarvelCharacterDetailsHeaderViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.addSubview(characterThumbnail)
-        view.addSubview(characterNameLabel)
+        configureStackView()
+        configure()
+    }
+    
+    private func configureStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(characterThumbnail)
+        stackView.addArrangedSubview(characterNameLabel)
+    }
+    
+    private func configure() {
+//        view.addSubview(characterThumbnail)
+//        view.addSubview(characterNameLabel)
         view.addSubview(favouriteButton)
-        
+        view.addSubview(stackView)
+//
         let padding: CGFloat = 20
-        
+//
         characterThumbnail.downloadImage(from: character.thumbnail.fullPath(thumbnailSize: .landscapeXLarge))
-        
+//
         characterNameLabel.text = character.name
         characterNameLabel.numberOfLines = 0
+//
+//        let characterThumbnailWidthAnchorConstraint = characterThumbnail.widthAnchor.constraint(equalToConstant: 150)
+//        let characterNameLabelLeadingAnchorConstraint = characterNameLabel.leadingAnchor.constraint(equalTo: characterThumbnail.trailingAnchor, constant: 12)
+//        characterThumbnailWidthAnchorConstraint.priority = UILayoutPriority(750)
+//        characterNameLabelLeadingAnchorConstraint.priority = UILayoutPriority(750)
         
-        let characterThumbnailWidthAnchorConstraint = characterThumbnail.widthAnchor.constraint(equalToConstant: 150)
-        let characterNameLabelLeadingAnchorConstraint = characterNameLabel.leadingAnchor.constraint(equalTo: characterThumbnail.trailingAnchor, constant: 12)
-        characterThumbnailWidthAnchorConstraint.priority = UILayoutPriority(750)
-        characterNameLabelLeadingAnchorConstraint.priority = UILayoutPriority(750)
-        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+//
         NSLayoutConstraint.activate([
-            characterThumbnail.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
-            characterThumbnail.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-            characterThumbnailWidthAnchorConstraint,
-            characterThumbnail.heightAnchor.constraint(equalTo: view.heightAnchor),
+//            characterThumbnail.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+//            characterThumbnail.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+//            characterThumbnailWidthAnchorConstraint,
+//            characterThumbnail.heightAnchor.constraint(equalTo: view.heightAnchor),
+//
+//            characterNameLabel.topAnchor.constraint(equalTo: characterThumbnail.topAnchor),
+//            characterNameLabelLeadingAnchorConstraint,
+//            characterNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+//            characterNameLabel.bottomAnchor.constraint(equalTo: characterThumbnail.bottomAnchor),
             
-            characterNameLabel.topAnchor.constraint(equalTo: characterThumbnail.topAnchor),
-            characterNameLabelLeadingAnchorConstraint,
-            characterNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
-            characterNameLabel.bottomAnchor.constraint(equalTo: characterThumbnail.bottomAnchor),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             
-            favouriteButton.topAnchor.constraint(equalTo: characterThumbnail.topAnchor, constant: -6),
-            favouriteButton.leadingAnchor.constraint(equalTo: characterThumbnail.leadingAnchor, constant: -6),
+
+            favouriteButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            favouriteButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             favouriteButton.widthAnchor.constraint(equalToConstant: 20),
             favouriteButton.heightAnchor.constraint(equalTo: favouriteButton.widthAnchor)
         ])
+        
+//        stackView.pinToEdges(of: view)
+        
     }
     
     private func setFavouriteState() {
